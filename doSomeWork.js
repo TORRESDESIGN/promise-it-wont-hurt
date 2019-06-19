4e78dd916e4c7218dbbe62bc0efb79c466e9951d
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2019-06-18 19:45:26 -0700

    finished all the excercises

--- doSomeWork.js
+++ doSomeWork.js
@@ -4,7 +4,26 @@ const http = require("q-io/http");
 
 http.read('http://localhost:7000')
 .then(id => {
-	return http.read('http://localhost:7000' + id)
+	return http.read('http://localhost:7001/' + id);
+})
+.then(json => {
+	console.log(JSON.parse(json));
 })
 .then(null, console.error)
 .done();
+
+/*-------Alternate Solution-------
+
+var qhttp = require('q-io/http');
+
+qhttp.read("http://localhost:7000/")
+.then(function (id) {
+  return qhttp.read("http://localhost:7001/" + id);
+})
+.then(function (json) {
+  console.log(JSON.parse(json));
+})
+.then(null, console.error)
+.done();
+
+*/
